0686d342e27b356409e9106c3b9e5c6362b8d420
Rwema <52289151+rwema3@users.noreply.github.com>
2022-06-03 08:38:10 +0200

    Update the main.dart

--- lib/widgets/the main.dart
+++ lib/widgets/the main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
           scaffoldBackgroundColor: kBackgroundColor,
   
           textTheme: TextTheme(
-            body
+            bod
           )),
       home: HomeScreen(),
     );
