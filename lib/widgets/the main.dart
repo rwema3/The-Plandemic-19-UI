372da8f6686686a69ee32bde235ee15180a4a8f3
Rwema <52289151+rwema3@users.noreply.github.com>
2022-06-03 11:44:45 +0200

    Update the main.dart

--- lib/widgets/the main.dart
+++ lib/widgets/the main.dart
@@ -172,7 +172,7 @@ class _HomeScreenState extends State<HomeScreen> {
                     children: <Widget>[
                       Text(
                         "Spread of Virus",
-                        style: kTityle,
+                        style: kTtyle,
                       
                   
                         "See details",
