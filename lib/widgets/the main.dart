b75a8e1632cb36b9b80c7258f3d70998f8139bd0
Rwema <52289151+rwema3@users.noreply.github.com>
2022-06-03 08:54:30 +0200

    Update the main.dart

--- lib/widgets/the main.dart
+++ lib/widgets/the main.dart
@@ -7,7 +7,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of you
+  // This widget is the root of yo
 
   
     
