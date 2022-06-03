e3bd647f5e98ca2a27e221c0624080f189b02748
Rwema <52289151+rwema3@users.noreply.github.com>
2022-06-03 15:35:45 +0200

    debugShowCheckedModeBanner: false,

--- lib/widgets/the main.dart
+++ lib/widgets/the main.dart
@@ -11,8 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowChecke
-      
+      debugShowCheckedModeBanner: false,
+      title: 'Covid 19'
       
           
   
