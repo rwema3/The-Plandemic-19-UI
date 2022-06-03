9e17b8d889c8ac66c63ae2d38253ff8044dd7f18
Rwema <52289151+rwema3@users.noreply.github.com>
2022-06-03 11:49:24 +0200

    Update the main.dart

--- lib/widgets/the main.dart
+++ lib/widgets/the main.dart
@@ -177,7 +177,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   
                       
                   
-                      a
+                      
                       fit: BoxFit.contain,
                     ),
                   ),
