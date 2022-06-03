cd99cb1b9d3eee4a3d42e00147a72eae1caa2db0
Rwema <52289151+rwema3@users.noreply.github.com>
2022-06-03 15:30:01 +0200

    Update the main.dart

--- lib/widgets/the main.dart
+++ lib/widgets/the main.dart
@@ -88,7 +88,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         'Japan'
                       ].map<DropdownMenuItem<String>>((String value) {
                         return DropdownMenuItem<String>(
-                          value: 
+                          value
                           child: Text(value),
                         );
                       }).toList(),
