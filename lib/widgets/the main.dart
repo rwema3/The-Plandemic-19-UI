b845051418fd193a64cf0bc92fb9cdde3a3a93a3
Rwema <52289151+rwema3@users.noreply.github.com>
2022-06-03 15:31:09 +0200

    Update the main.dart

--- lib/widgets/the main.dart
+++ lib/widgets/the main.dart
@@ -184,7 +184,16 @@ class _HomeScreenState extends State<HomeScreen> {
                     ],
                   ),
                   Container(
-                   
+                    margin: EdgeInsets.only(top: 20),
+                    padding: EdgeInsets.all(20),
+                    height: 178,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                      
                     child: Image.asset(
                       "assets/images/map.png",
                       fit: BoxFit.contain,
