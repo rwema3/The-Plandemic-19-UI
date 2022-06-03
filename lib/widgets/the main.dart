6cfd2b66bec03c8ac53cb2f3c3e0b7ae8a76a273
Rwema <52289151+rwema3@users.noreply.github.com>
2022-06-03 08:49:25 +0200

    Update the main.dart

--- lib/widgets/the main.dart
+++ lib/widgets/the main.dart
@@ -9,7 +9,7 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
-  Widget build(Build
+  Widget build(Buil
     
       
       
