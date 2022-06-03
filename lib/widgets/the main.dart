30df5021619a7b70bed75e0a936ee30f68b8825d
Rwema <52289151+rwema3@users.noreply.github.com>
2022-06-03 08:45:06 +0200

    Update the main.dart

--- lib/widgets/the main.dart
+++ lib/widgets/the main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Covid 19
+      title: 'Covid 1
       
           
   
