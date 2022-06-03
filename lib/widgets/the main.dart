da411a34fe40d994235eeaaae9c1d335cdd88502
Rwema <52289151+rwema3@users.noreply.github.com>
2022-06-03 08:39:51 +0200

    Update the main.dart

--- lib/widgets/the main.dart
+++ lib/widgets/the main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Covid 19',
       theme: ThemeData(
-          scaffoldBackg
+          scaffoldBack
   
           textTheme: TextTheme(
       
