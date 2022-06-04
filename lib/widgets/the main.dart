c16283e849660b0dbac16d127e9a0c61602266fb
Rwema <52289151+rwema3@users.noreply.github.com>
2022-06-04 21:51:58 +0200

    void main() => runApp(MyApp());

--- lib/widgets/the main.dart
+++ lib/widgets/the main.dart
@@ -4,6 +4,7 @@ import 'package:covid_19/widgets/my_header.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
+void main() => runApp(MyApp());
 
           
           
