4415440d4aa239f190cc9f4a48794f9c0a000e8d
Rwema <52289151+rwema3@users.noreply.github.com>
2022-06-04 21:51:17 +0200

    });   }

--- lib/widgets/the main.dart
+++ lib/widgets/the main.dart
@@ -38,6 +38,8 @@ class _HomeScreenState extends State<HomeScreen> {
   void onScroll() {
     setState(() {
       offset = (controller.hasClients) ? controller.offset : 0;
+    });
+  }
   Widget build(BuildContext context) {
             MyHeader(
               image: "assets/icons/Drcorona.svg",
