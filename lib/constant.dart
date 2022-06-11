ffe411211b9797e4f7faab64f2b9d3cacf0703cc
Rwema <52289151+rwema3@users.noreply.github.com>
2022-06-12 00:07:43 +0200

    Update constant.dart

--- lib/constant.dart
+++ lib/constant.dart
@@ -9,6 +9,7 @@ const kInfectedColor = Color(0xFFFF8748);
 const kDeathColor = Color(0xFFFF4848);
 const kRecovercolor = Color(0xFF36C12C);
 const kPrimaryColor = Color(0xFF3382CC);
+final kShadowColor = Color(0xFFB7B7B7).withOpacity(.16);
 
 // Text Style
 const kHeadingTextStyle = TextStyle(
