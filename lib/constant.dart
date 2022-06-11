98672bca017872806ea44aecb1f6be0f07b9f7c6
Rwema <52289151+rwema3@users.noreply.github.com>
2022-06-12 00:07:46 +0200

    Update constant.dart

--- lib/constant.dart
+++ lib/constant.dart
@@ -10,6 +10,7 @@ const kDeathColor = Color(0xFFFF4848);
 const kRecovercolor = Color(0xFF36C12C);
 const kPrimaryColor = Color(0xFF3382CC);
 final kShadowColor = Color(0xFFB7B7B7).withOpacity(.16);
+final kActiveShadowColor = Color(0xFF4056C6).withOpacity(.15);
 
 // Text Style
 const kHeadingTextStyle = TextStyle(
