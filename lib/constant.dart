3b1d16850e06ed3d73d1aab4e14e34c20d008013
Rwema <52289151+rwema3@users.noreply.github.com>
2022-06-12 00:07:29 +0200

    Update constant.dart

--- lib/constant.dart
+++ lib/constant.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 // Colors
 const kBackgroundColor = Color(0xFFFEFEFE);
 const kTitleTextColor = Color(0xFF303030);
+const kBodyTextColor = Color(0xFF4B4B4B);
 
 // Text Style
 const kHeadingTextStyle = TextStyle(
