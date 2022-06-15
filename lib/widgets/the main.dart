61e4a7ea8dc27bfbe01c4b3b86ae48e237bcceee
Rwema <52289151+rwema3@users.noreply.github.com>
2022-06-15 10:59:28 +0200

    Update the main.dart

--- lib/widgets/the main.dart
+++ lib/widgets/the main.dart
@@ -10,8 +10,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
    
 class HomeScreen extends StatefulWidget {
   @override
