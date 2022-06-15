f678dccb47120f0a03de922f090613f82825e514
Rwema <52289151+rwema3@users.noreply.github.com>
2022-06-15 11:03:43 +0200

    Update the main.dart

--- lib/widgets/the main.dart
+++ lib/widgets/the main.dart
@@ -2,8 +2,6 @@ import 'package:covid_19/constant.dart';
 import 'package:covid_19/widgets/counter.dart';
 import 'package:covid_19/widgets/my_header.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
