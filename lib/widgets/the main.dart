f7a7fe1183c4a1b333a3aa633e9b1a606ee3ad0a
Rwema <52289151+rwema3@users.noreply.github.com>
2022-06-15 11:02:48 +0200

    Update the main.dart

--- lib/widgets/the main.dart
+++ lib/widgets/the main.dart
@@ -10,7 +10,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-   
+    super.initState();
+    controller.addListener(onScroll);
+  }
 
   @override
   void dispose() {
