ef804f7c7c0c37bff6175cb74cd78d5162fc67f3
Rwema <52289151+rwema3@users.noreply.github.com>
2022-06-15 10:59:32 +0200

    Update the main.dart

--- lib/widgets/the main.dart
+++ lib/widgets/the main.dart
@@ -11,10 +11,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
    
-class HomeScreen extends StatefulWidget {
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
 
 class _HomeScreenState extends State<HomeScreen> {
   final controller = ScrollController();
